574926773df2b7585e9b98324f2b66890e7ddbeb
Joaquin Ignacio Fritz Carrizo <d48125437@alumnos.itr.edu.ar>
2024-08-08 11:30:16 -0300

    Ejercicio de Programacion

--- Comienzos_vite/src/App.jsx
+++ Comienzos_vite/src/App.jsx
@@ -1,38 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [text, setText] = useState("");
+	const [products, setProducts] = useState([]);
+	const [filtered, setFiltered] = useState([]);
+	const [text, setText] = useState("");
 
-  const search = (event) => {
-    setText(event.target.value);
+	const search = (event) => {
+		setText(event.target.value);
+		if (setText === "") {
+			setProducts(filtered);
+		} else {
+			setProducts(
+				filtered.filter((product) => product.title.startsWith(text))
+			);
+			console.log(text);
+		}
+	};
 
-    console.log(
-      products.filter((product) => {
-        product.title.startsWith(text);
-      })
-    );
-  };
+	useEffect(() => {
+		fetch("https://fakestoreapi.com/products")
+			.then((response) => response.json())
+			.then((json) => {
+				setProducts(json);
+				setFiltered(json);
+			});
+	}, []);
 
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((json) => {
-        setProducts(json);
-      });
-  }, []);
-  return (
-    <>
-      <h1>Comenzando React</h1>
-      <input onChange={search}></input>
-      <ul>
-        {products.map((product) => {
-          <li key={product.id}>{product.title}</li>;
-        })}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<h1>Comenzando React</h1>
+			<input onChange={search} value={text}></input>
+			<ul>
+				{products.map((product) => (
+					<li key={product.id}>{product.title}</li>
+				))}
+			</ul>
+		</>
+	);
 }
 
 export default App;
